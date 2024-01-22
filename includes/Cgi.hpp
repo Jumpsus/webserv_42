a0c1d278f28b273e2fb9e833e95452cc7401fcb0
pingpanu <dingpanuwong@gmail.com>
2024-01-23 00:21:05 +0700

    make new cgi

--- includes/Cgi.hpp
+++ includes/Cgi.hpp
@@ -6,6 +6,7 @@
 # include "Request.hpp"
 # include "config/Config.hpp"
 # include "Webserv.hpp"
+# include "Response.hpp"
 # define env_iter std::map<std::string, std::string>::const_iterator
 
 /* refer to rfc3875 */
@@ -18,7 +19,7 @@ class CgiHandler {
 
         CgiHandler &operator=(CgiHandler const &cgi);
 
-        std::string                         execCgi(const std::string& script, int &error);
+        void                                execCgi(Request &req, const Location& loc_ptr);
         const std::string&                  getBody();
         void                                setBody(const std::string& ebody);
         void                                printEnv();
@@ -27,13 +28,18 @@ class CgiHandler {
         void                                setCgiPath(const std::string& cgi_path);
         const std::string&                  getCgiPath() const;
 
+        int                                 pipe_in[2]; //to get input body throw by response
+        int                                 pipe_out[2]; //to send output from cgi to build response
+
     private:
         std::map<std::string, std::string>  _env;
         std::string                         _body;
         std::string                         _cgiPath;
+        char**                              _args;
 
         std::string                         _getPathInfo(const std::string& req_path, const std::vector<std::string>& exts);
         std::string                         _decodeQuery(std::string req_query);
+        std::string                         _getScriptFilename(const std::string& cgi_path);
         void                                _initEnv(Request& request, Location& loc_ptr, const std::string& host);
         void                                _setEnv(Request& request, Location& loc_ptr, const std::string& host);
         char**                              _envToCstrArr() const;
