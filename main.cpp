f8e44c7933b5cf3c00aef2f524a34d0036d29ce4
peerapat.r <peerapat.r@kbtg.tech>
2024-02-01 22:37:49 +0700

    fix main

--- main.cpp
+++ main.cpp
@@ -14,41 +14,16 @@ int main(int ac, char **av)
     try 
     {
         Config conf(config_path);
-        //conf.printConfigInfo();
-        servers = conf.getServers();
-        ServerManager server_manager(servers);
-        server_manager.setServers();
-        server_manager.startServers();
     }
     catch(std::string error) 
     {  
         std::cout << RED << "error: " << error << RESET << std::endl;
+        return (1);
     }
 
-    //for (std::vector<Server>::iterator it = servers.begin(); it != servers.end(); it++)
-    //{
-    //    it->printServerInfo();
-    //}
-    // std::string content = c.getContent();
-
-    // std::cout<< content <<std::endl;
-
-    // std::string block;
-
-    // int index = 0;
-
-    // while (ft_is_white_space(content[index]))
-    // {
-    //     index++;
-    // }
-
-    // std::cout << "index = " << index <<std::endl;
-    // block = findBlock(content.substr(index, content.length()), "location");
-    
-    // if (block.length() == 0){
-    //     std::cout << "get block fail" << std::endl;
-    // }
-
-    // std::cout << "block = " << block << std::endl;
-    // std::cout << "content = " << content << content.length() << std::endl;
+    //conf.printConfigInfo();
+    servers = conf.getServers();
+    ServerManager server_manager(servers);
+    server_manager.setServers();
+    server_manager.startServers();
 }
